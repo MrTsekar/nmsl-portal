a9a4735dbaf8bbd97fa99fce4dca69cd26dd1c65
mrtsekar <mr.tsekar01@gmail.com>
2026-05-04 09:55:25 +0100

    Allow remote image hosts (Azure Blob) in next/image optimizer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.blob.core.windows.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
